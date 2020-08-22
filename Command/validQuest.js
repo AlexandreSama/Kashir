81d3642c5f85449c6eac49d7667ced89bf201b3c
alexandresama <alexandrehainy12@gmail.com>
2020-08-23 00:18:29 +0200

    Ajout showAccount et validQuest, code réctifié

---
+++ Command/validQuest.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const mysql = require('mysql');
+
+module.exports.run = (client, message, args) => {
+    
+}
+
+module.exports.help = {
+    name: 'validquest'
+};
