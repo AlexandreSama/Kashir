eacea4dc1f7c4b9d9662cc26658bc0fed868a4e2
alexandresama <alexandrehainy12@gmail.com>
2020-08-26 16:14:50 +0200

    Ajout ShowTraders

---
+++ Command/showTraders.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const mysql = require('mysql');
+const config = require('../config.json');
+
+module.exports.run = (client, message, args) => {
+
+    //Variable de connexion a la BDD
+    var connection = mysql.createConnection({
+        host: config.ip,
+        user : config.user,
+        password: config.password,
+        database: config.database,
+    });
+
+    //Connexion a la BDD
+    connection.connect(console.log("Connexion Réussi"));
+
+    connection.query(`SELECT name FROM traders`, function(error, result) {
+        if(error){
+            console.log(error);
+            message.channel.send("Erreur, veuillez contacter votre Administrateur !")
+        }
+        if(result){
+            console.log(result);
+            //On rend sous forme de String le résultat
+            var data = JSON.stringify(result)
+            //On rend sous forme de JSON la var data
+            var finalData = JSON.parse(data)
+            finalData.forEach(function(data, index) {
+                message.channel.send(`- nom : ${data.name}` )
+            })
+        }
+    })
+
+}
+
+module.exports.help = {
+    name: 'showtraders'
+};
