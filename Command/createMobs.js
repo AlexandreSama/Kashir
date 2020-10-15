431a980685713d98864d4987a502368e594f7e60
Alexandre <alexandrehainy12@gmail.com>
2020-10-15 10:30:09 +0200

    always trying to resolve createMobs bug

--- Command/createMobs.js
+++ Command/createMobs.js
@@ -10,12 +10,8 @@ module.exports.run = (client, message) => {
             .on('error', console.error)
             .pipe(fs.createWriteStream('meme.png'));
     }
-
-    if(message.attachments.first()){//checks if an attachment is sent
-        if(msg.attachments.first().filename === `png`){//Download only png (customize this)
-            download(msg.attachments.first().url);//Function I will show later
-        }
-    }
+    
+    download(message.attachments.first().url);//Function I will show later
 }
 
 module.exports.help = {
