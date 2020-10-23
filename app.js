28737a16140b28f35f1f3f8e16375358559d0c72
AlexandreSama <alexandrehainy12@gmail.com>
2020-10-24 00:12:48 +0200

    remove package and change style from the code

--- app.js
+++ app.js
@@ -1,80 +1,86 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const fs = require('fs');
-const prefix = "!";
+const prefix = '!';
 const db = require('quick.db');
 
-let infos = [];
+const infos = [];
 
 fs.readdir('./Command/', (error, f) => {
-    if (error) { return console.error(error); }
-        let commandes = f.filter(f => f.split('.').pop() === 'js');
-        if (commandes.length <= 0) { return console.log('Aucune commande trouvée !'); }
+  if (error) {
+    return console.error(error);
+  }
+  const commandes = f.filter((f) => f.split('.').pop() === 'js');
+  if (commandes.length <= 0) {
+    return console.log('Aucune commande trouvée !');
+  }
 
-        commandes.forEach((f) => {
-            let commande = require(`./Command/${f}`);
-            console.log(`${f} commande chargée !`);
-            client.commands.set(commande.help.name, commande);
-        });
-        infos.push(`${commandes.length}`)
+  commandes.forEach((f) => {
+    const commande = require(`./Command/${f}`);
+    console.log(`${f} commande chargée !`);
+    client.commands.set(commande.help.name, commande);
+  });
+  infos.push(`${commandes.length}`);
 });
 
 fs.readdir('./Events/', (error, f) => {
-    if (error) { return console.error(error); }
-        console.log(`${f.length} events chargés`);
+  if (error) {
+    return console.error(error);
+  }
+  console.log(`${f.length} events chargés`);
 
-        f.forEach((f) => {
-            let events = require(`./Events/${f}`);
-            let event = f.split('.')[0];
-            client.on(event, events.bind(null, client, fs));
-        });
+  f.forEach((f) => {
+    const events = require(`./Events/${f}`);
+    const event = f.split('.')[0];
+    client.on(event, events.bind(null, client, fs));
+  });
 });
 
-client.on('ready', ready=>{
-    infos.push(client.guilds.cache.size);
-    fs.writeFile('infos.json', JSON.stringify(infos), (err) => {
-        if (err) throw err;
-        console.log('Le fichier a été sauvegarder!');
-    });
-})
+client.on('ready', (ready)=>{
+  infos.push(client.guilds.cache.size);
+  fs.writeFile('infos.json', JSON.stringify(infos), (err) => {
+    if (err) throw err;
+    console.log('Le fichier a été sauvegarder!');
+  });
+});
 
 function xp(message) {
-        const randomNumber = Math.floor(Math.random() * 10) + 15;
-        db.add(`guild_${message.guild.id}_xp_${message.author.id}`, randomNumber)
-        db.add(`guild_${message.guild.id}_xptotal_${message.author.id}`, randomNumber)
-        var level = db.get(`guild_${message.guild.id}_level_${message.author.id}`) || 1
-        var xp = db.get(`guild_${message.guild.id}_xp_${message.author.id}`)
-        var xpNeeded = level * 500
-        if(xpNeeded < xp){
-            var newLevel = db.add(`guild_${message.guild.id}_level_${message.author.id}`, 2)
-            db.subtract(`guild_${message.guild.id}_xp_${message.author.id}`, xpNeeded)
-            message.channel.send(`${message.author}, tu est monté au niveau ${newLevel}`) 
-        }
-
+  const randomNumber = Math.floor(Math.random() * 10) + 15;
+  db.add(`guild_${message.guild.id}_xp_${message.author.id}`, randomNumber);
+  db.add(`guild_${message.guild.id}_xptotal_${message.author.id}`, randomNumber);
+  const level = db.get(`guild_${message.guild.id}_level_${message.author.id}`) || 1;
+  const xp = db.get(`guild_${message.guild.id}_xp_${message.author.id}`);
+  const xpNeeded = level * 500;
+  if (xpNeeded < xp) {
+    const newLevel = db.add(`guild_${message.guild.id}_level_${message.author.id}`, 2);
+    db.subtract(`guild_${message.guild.id}_xp_${message.author.id}`, xpNeeded);
+    message.channel.send(`${message.author}, tu est monté au niveau ${newLevel}`);
+  }
 }
 
-client.on('message', async message => {
-    
-    let messageArray = message.content.split(/\s+/g);
-    let command = messageArray[0]
-    let args = messageArray.slice(1)
+client.on('message', async (message) => {
+  const messageArray = message.content.split(/\s+/g);
+  const command = messageArray[0];
+  const args = messageArray.slice(1);
 
-    if (!command.startsWith(prefix)) return;
+  if (!command.startsWith(prefix)) return;
 
-    let cmd = client.commands.get(command.slice(prefix.length))
-    if (cmd) cmd.run(client, message, args);
-    if (message.author.bot) {return;}
+  const cmd = client.commands.get(command.slice(prefix.length));
+  if (cmd) cmd.run(client, message, args);
+  if (message.author.bot) {
+    return;
+  }
 
-    xp(message)
-
+  xp(message);
 });
 
-client.on('guildMemberAdd', member => {
-
-    console.log(`L'utilisateur'` + member.user.tag + `a rejoins le serveur!`);
-    var role = member.guild.roles.cache.find(role => role.name === 'Les copains');
-    member.roles.add(role);
-
-})
+client.on('guildMemberAdd', (member) => {
+  console.log(`L'utilisateur'` + member.user.tag + `a rejoins le serveur!`);
+  const role = member.guild.roles.cache.find((role) => role.name === 'Les copains');
+  member.roles.add(role);
+});
 client.login('');
