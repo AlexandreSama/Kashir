5215aaf18da751dbe3a7ce1d04501b5679753a0d
AlexandreSama <alexandrehainy12@gmail.com>
2020-10-24 15:11:41 +0200

    trying to fix guildRemove

--- Events/guildMemberRemove.js
+++ Events/guildMemberRemove.js
@@ -1,23 +1,27 @@
-const Canvas = require("discord-canvas");
+const canvas = require("discord-canvas"),
+  goodbyeCanvas = new canvas.Goodbye();
 const Discord = require("discord.js");
 const guild = new Discord.Guild();
 
-module.exports = async (client, member, message) => {
+module.exports = async (client, message) => {
 
-    let user = member.user;
+    let channelName = 'goodbye';
+    const channel = client.channels.cache.find(channel => channel.name === channelName)
 
-    const image = await new Canvas.Goodbye()
-        .setUsername(user.username)
-        .setDiscriminator(user.tag)
-        .setMemberCount(guild.memberCount)
-        .setGuildName(guild.name)
-        .setColor("border", "#8015EA")
-        .setColor("username-box", "#8015EA")
-        .setColor("discriminator-box", "#8015EA")
-        .setColor("message-box", "#8015EA")
-        .setColor("title", "#8015EA")
-        .toAttachment();
- 
-    message.channel.send(new Discord.MessageAttachment(image, "RankCard.png"));
+    let image = await goodbyeCanvas
+    .setUsername(message.author.username)
+    .setDiscriminator(message.author.tag)
+    .setMemberCount(guild.memberCount)
+    .setGuildName(guild.name)
+    .setColor("border", "#8015EA")
+    .setColor("username-box", "#8015EA")
+    .setColor("discriminator-box", "#8015EA")
+    .setColor("message-box", "#8015EA")
+    .setColor("title", "#8015EA")
+    .toAttachment();
+
+    let attachment = new MessageAttachment(image.toBuffer(), "goodbye-image.png");
+  
+    channel.send(attachment)
 
 };
