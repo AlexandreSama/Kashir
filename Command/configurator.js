df077265c68464b509a321319a526b0e8b58ff67
AlexandreSama <alexandrehainy12@gmail.com>
2020-12-29 23:21:52 +0100

    add configurator cmd, remove createDatabase cmd

--- Command/configurator.js
+++ Command/configurator.js
@@ -6,67 +6,77 @@ module.exports.run = (client, message) => {
 
     message.delete();
 
-    let config = [];
+    // Const pour retenir le nom de la commande (on ajoute un espace a la fin pour ne pas mal découpé le message)
+    const commandName = "!config ";
 
-    message.channel.send("On va commencer la configuration du bot !");
+    // on retire le commandName du message et on découpe chaque String a chaque |
+    let messageArray = message.content.substring(commandName.length).split(" | ");
 
-    message.channel.send("Peut-tu me donner l'adresse (ip) de la Base de Données ?");
+    // On crée un array vide
+    let infos = [];
 
-        if (message.author.bot) {
-            return;
+    //On push dans le tableau les infos que l'on veut
+    infos.push({ip: messageArray[0], user: messageArray[1], password: messageArray[2], database: messageArray[3], ficheCategoryName: messageArray[4], staffCategoryName: [5] })
+
+    // Variable de connexion a la BDD
+    if (infos[0]['password'].length <= 0) {
+        var connection = mysql.createConnection({
+            host: `${messageArray[0]}`,
+            user : `${messageArray[1]}`,
+        })
+    }else if (infos[0]['password'].length > 0) {
+        var connection = mysql.createConnection({
+            host: `${messageArray[0]}`,
+            user : `${messageArray[1]}`,
+            password: `${messageArray[2]}`,
+        })
+    }
+
+    //Connexion a la BDD
+    connection.connect(function(err, res) {
+        if (err) {
+          console.error('error connecting: ' + err.stack);
+          return;
         }
-        let lastMessage = message.channel.lastMessage;
-        if(lastMessage.author.bot === true){
-            console.log("ok")
-        }else {
-            config.push(lastMessage.content)
-            message.channel.send("Maintenant, donne moi le nom d'utilisateur de la Base De Données !");
-        }
-        if(config.length === 1){
-            if(lastMessage.author.bot === true){
-                console.log("ok")
-            }else {
-                config.push(lastMessage.content)
-                message.channel.send("Maintenant, donne moi le mot de passe de la Base De Données !");
-            }
-        }
-        if(config.length === 2){
-            if(lastMessage.author.bot === true){
-                console.log("ok")
-            }else {
-                config.push(lastMessage.content)
-                message.channel.send("Maintenant, donne moi le nom de la Base De Données !");
-            }
-        }
-        if(config.length === 3){
-            if(lastMessage.author.bot === true){
-                console.log("ok")
-            }else {
-                config.push(lastMessage.content)
-                message.channel.send("Maintenant, donne moi le nom du channel de bienvenue et de leave !");
-            }
-        }
-        if(config.length === 4){
-            if(lastMessage.author.bot === true){
-                console.log("ok")
-            }else {
-                config.push(lastMessage.content)
-                message.channel.send("Maintenant, donne moi le nom de la catégorie ou seront les fiches en attentes !");
-            }
-        }
-        if(config.length === 5){
-            if(lastMessage.author.bot === true){
-                console.log("ok")
-            }else {
-                config.push(lastMessage.content)
-                message.channel.send("Voila ! Je suis parfaitement configuré ! C'étais rapide !")
-                let datas = JSON.stringify(config);
-                fs.writeFile("conf.json", datas, (err) => {
-                    if (err) throw err;
-                    console.log('Data written to file');
-                });
-            }
-        }
+    })
+
+    //Création de toute la Base de Données et sauvegarde des infos sur le Project Directory !
+    connection.query(`CREATE DATABASE ${messageArray[3]};`, function(error, result){
+        if(error){
+            connection.end();
+            console.log(error);
+        }if(result){
+            connection.query(`USE ${messageArray[3]};`, function(error, result){
+        if(error){
+            connection.end();
+            console.log(error);
+        }if(result){
+            connection.query(`CREATE TABLE items (ID INT NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, description TEXT NOT NULL, PRIMARY KEY (ID));`, function(error, result){
+        if(error){
+            connection.end();
+            console.log(error);
+        }if(result){
+            connection.query(`CREATE TABLE bank (ID INT NOT NULL AUTO_INCREMENT, idaccount INT NOT NULL, money INT, PRIMARY KEY (ID));`, function(error, result){
+        if(error){
+            connection.end();
+            console.log(error);
+        }if(result){
+            connection.query(`CREATE TABLE quests (ID INT NOT NULL AUTO_INCREMENT, nom VARCHAR(200) NOT NULL, description TEXT NOT NULL, recompense TEXT NOT NULL, PRIMARY KEY (ID))`, function(error, result){
+        if(error){
+            connection.end();
+            message.channel.send(error)
+        }if(result){
+            connection.query(`CREATE TABLE fiche (ID INT NOT NULL AUTO_INCREMENT, idaccount BIGINT NOT NULL, nom VARCHAR(200) NOT NULL, prenom VARCHAR(200) NOT NULL, sexe VARCHAR(200) NOT NULL, age INT NOT NULL, race VARCHAR(200) NOT NULL, physique TEXT NOT NULL, mental TEXT NOT NULL, pouvoirs INT NOT NULL, PRIMARY KEY (ID))`, function(error, result){
+        if(error){
+            connection.end();
+            console.log(error)
+            message.channel.send('Erreur ! Veuillez contacter votre administrateur')
+        }if(result){
+            console.log("yes !")
+            message.channel.send("Base de Donnée crée avec succés et informations de la BDD save !")
+            let data = JSON.stringify(infos);
+            fs.writeFileSync('config.json', data);
+        }})}})}})}})}})}})
 
 }
 
--- Events/ready.js
+++ Events/ready.js
@@ -3,4 +3,5 @@ module.exports = (client) => {
     console.log("Bot Prêt");
 
     client.user.setActivity("Le Necronomicon pour les nuls", {type: 'PLAYING'})
+
 };
