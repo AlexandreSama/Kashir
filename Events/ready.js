a4f97cac3d76fc0a3eb98a30b9cb31854baa1721
alexandre <alexandrehainy12@gmail.com>
2021-01-08 10:09:40 +0100

    Add Weird Status

--- Events/ready.js
+++ Events/ready.js
@@ -2,6 +2,11 @@ module.exports = (client) => {
     
     console.log("Bot Prêt");
 
-    client.user.setActivity("Le Necronomicon pour les nuls", {type: 'PLAYING'})
-
+    setInterval(() => {
+    	let servernumber = client.guilds.cache.size;
+    	let usersnumber = client.users.cache.size;
+    	let activiter = [`Actuellement sur ${servernumber} serveurs auquels je suis attaché!`, `Je compte ${usersnumber} membres m'utilisant !`]
+    	const index = Math.floor(Math.random() * (activiter.length - 1) + 1);
+    	client.user.setActivity(activiter[index], 'WATCHING');
+    }, 15000);
 };
