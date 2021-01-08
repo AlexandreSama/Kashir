398babdbe2e6b701e94eed7d04857a518d7235c0
alexandre <alexandrehainy12@gmail.com>
2021-01-08 10:37:54 +0100

    Fix Ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -2,11 +2,12 @@ module.exports = (client) => {
     
     console.log("Bot Prêt");
 
+    let servernumber = client.guilds.cache.size;
+    let usersnumber = client.users.cache.size;
+
     setInterval(() => {
-    	let servernumber = client.guilds.cache.size;
-    	let usersnumber = client.users.cache.size;
-    	let activiter = [`Actuellement sur ${servernumber} serveurs auquels je suis attaché!`, `Je compte ${usersnumber} membres m'utilisant !`]
+    	let activiter = [`Actuellement sur ${servernumber} serveurs auquels je suis attaché!`, `Je compte ${usersnumber} membres m'utilisant !`, `Mon Créateur a dit que j'étais gratuit !`, `J'ai même une IA pour m'aider, si c'est pas la classe ca !`, `Chaque bug est une cannette de Coca vide ! Mon disque dur était plein a craquer`, `Je suis encore en plein développement alors attention a moi !`, `Je suis mis a jour sur Github tout les soirs a 19h, pensez a me mettre a jour svp`]
     	const index = Math.floor(Math.random() * (activiter.length - 1) + 1);
-    	client.user.setActivity(activiter[index], 'WATCHING');
+    	client.user.setActivity(activiter[index], { type: 'WATCHING'});
     }, 15000);
 };
